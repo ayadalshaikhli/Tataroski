864df17badece351ef7cb1bc3be4feae82c23444
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2022-04-13 03:26:20 -0700

    vercal problem

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -13,7 +13,7 @@ const ProductCard = ({ product }) => {
     <Link href={`/products/${handle}`}>
       <a className="group ">
         <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
-          <div className="relative group-hover:opacity-75 h-72 my">
+          <div className="relative group-hover:opacity-75 h-72">
             <Image
               src={originalSrc}
               alt={altText}
